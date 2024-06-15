fe6e90aab7241b2f4fb81148edcc7f816956e768
Hunter W <kfateweaver@gmail.com>
2024-06-15 00:28:39 -0400

    heroku fix

--- public/client/config.js
+++ public/client/config.js
@@ -1,4 +1,4 @@
-export const APP_PORT = 3050
-export const API_PORT = 3051
-export const DEBUG_MODE = "ERROR"
+export const APP_PORT = 3050;
+export const API_PORT = 3051;
+export const DEBUG_MODE = "ERROR";
 export const COOKIE_SAVE_TIME_DAYS = 30;
